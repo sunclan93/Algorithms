430d3f1bf60101c82ae0b238d4d6c1ae0dde9401
sutianru-at-339974164667 <sutianru@amazon.com>
2021-12-12 21:09:26 -0500

    update best eim quest

--- src/Algorithms/Lintcode/DataStructure/BestTimeToBuyAndSellStock.java
+++ src/Algorithms/Lintcode/DataStructure/BestTimeToBuyAndSellStock.java
@@ -17,4 +17,24 @@ public class BestTimeToBuyAndSellStock {
         }
         return res;
     }
+    /*
+    *  Not using prefix arrays
+    * */
+    public int maxProfitV2(int[] prices) {
+        // write your code here
+        int[] prefix = new int[prices.length];
+        int res = 0;
+        int pre = Integer.MAX_VALUE;
+        for (int i = 0; i < prices.length; i++) {
+            if (i == 0) {
+                pre = prices[i];
+            }
+            else {
+                res = Math.max(res, prices[i] - pre);
+                pre = Math.min(pre, prices[i]);
+            }
+
+        }
+        return res;
+    }
 }
